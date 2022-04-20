23edd8a02d6e646f3eead056d025616d01119d6f
Dario Anguilla <dario.anguilla@gmail.com>
2022-04-20 10:42:52 +0200

    public products

--- src/main/java/com/arces/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/arces/ecommerce/config/SecurityConfig.java
@@ -45,10 +45,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/authenticate")
+        
+        http.csrf().disable().authorizeRequests()
+        .antMatchers("/products").permitAll()
+        .and().authorizeRequests().antMatchers("/authenticate")
                 .permitAll().anyRequest().authenticated()
                 .and().exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
