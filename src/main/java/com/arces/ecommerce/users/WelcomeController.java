f6df2961a0f5f216e40aa92ec3257356daf96414
Dario Anguilla <dario.anguilla@gmail.com>
2022-04-20 12:05:35 +0200

    controller

--- src/main/java/com/arces/ecommerce/users/WelcomeController.java
+++ src/main/java/com/arces/ecommerce/users/WelcomeController.java
@@ -3,12 +3,15 @@ package com.arces.ecommerce.users;
 import com.arces.ecommerce.util.JwtUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.Nullable;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class WelcomeController {
@@ -30,7 +33,7 @@ public class WelcomeController {
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
         } catch (Exception ex) {
-            throw new Exception("Invalid username/password");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Invalid username/password");
         }
         return jwtUtil.generateToken(authRequest.getUsername());
 
